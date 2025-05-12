3f0741bffe0e84f7c597ec503a11bf68381dcd29
este <djjd2000@naver.com>
2025-05-12 22:02:22 +0900

    Feature: implement red test of signIn(add stub of AuthService factory, test of POST /auth/session)

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,14 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { UserInfoDto } from 'src/user/dto/user-info.dto';
+
+export type StubAuthService = {
+  signIn: jest.Mock<Promise<UserInfoDto>, [SignInDto]>;
+};
+
+const createMockAuthService = (): StubAuthService => ({
+  signIn: jest.fn<Promise<UserInfoDto>, [SignInDto]>(),
+});
+
+const MOCK_USER_PROPS = {
+  id: 'some-uuid',
+  userId: 'someUserId',
+  username: 'someUsername',
+  password: 'Password123!',
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let mockAuthService: StubAuthService;
 
   beforeEach(async () => {
+    mockAuthService = createMockAuthService();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -16,5 +41,19 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('POST /auth/session', () => {
+    it('should return HttpStatus.OK and UserInfoDto if available user credential is provided', async () => {
+      const { id, password, userId, username } = MOCK_USER_PROPS;
+      const mockSignInDto: SignInDto = { password, userId };
+      const mockUserInfoDto: UserInfoDto = { id, userId, username };
+      mockAuthService.signIn.mockResolvedValue(mockUserInfoDto);
+
+      const result = await controller.signIn(mockSignInDto);
+
+      expect(mockAuthService.signIn).toHaveBeenCalledWith(mockSignInDto);
+      expect(result).toEqual(mockUserInfoDto);
+    });
   });
 });
