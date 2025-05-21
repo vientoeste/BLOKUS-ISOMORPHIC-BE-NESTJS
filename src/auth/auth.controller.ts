e2c13382fdee7b1ca4ea69a3e2f0d507cd99714e
este <djjd2000@naver.com>
2025-05-21 23:42:28 +0900

    Feature: add controller method of POST /auth/session

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
+
+  @Post('session')
+  async signIn(
+    @Body() signInDto: SignInDto
+  ) {
+    const result = await this.authService.signIn(signInDto);
+    return result;
+  }
 }
